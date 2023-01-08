61a82a0967ef9d0705a5e12ef99fb8974c643c06
Sandy Cheng <cheek.codes@gmail.com>
2023-01-07 21:59:12 -0500

    yt dld

--- gui.py
+++ gui.py
@@ -1,14 +1,15 @@
-from os import path
 from tkinter import *
 from tkinter import filedialog
 from moviepy import *
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
 
+import shutil
 
-# functions
+
+# Functions
 def select_path():
-    # allows user to select path
+    #allows user to select a path from the explorer
     path = filedialog.askdirectory()
     path_label.config(text=path)
 
@@ -16,41 +17,45 @@ def download_file():
     #get user path
     get_link = link_field.get()
     #get selected path
-    user_path = path_label.cget("text")
+    user_path = path_field.get()
+    user_path - path_label.cget("text")
+    screen.title("Downloading..")
     #download video
-    mp4_video = Youtube(get_link).streams.get_highest_resolution().download()
-    vid_clip = VideoFileClip(mp4_video)
+    mp4_video = YouTube(get_link).streams.get_highest_resolution().download()
     vid_clip.close()
+    shutil.move(mp4_video, user_path)
+    screen.title("Download Complet!")
 
 screen = Tk()
-title = screen.title('Youtube Download')
+title = screen.title("Youtube Downloader")
 canvas = Canvas(screen, width=500, height=500)
 canvas.pack()
 
 #image logo
-logo_img = PhotoImage(file='youtube_downloader\yt.png')
+logo_img = PhotoImage(file="yt.png")
 #resize
 logo_img = logo_img.subsample(2, 2)
 canvas.create_image(250, 80, image=logo_img)
 
 #link field
-link_field = Entry(screen, width=40, font=('Arial', 15) )
-link_label = Label(screen, text="Enter Download Link: ", font=('Arial', 15))
+link_field - Entry(screen, width=40, font=("Arial", 15))
+link_label = Label(screen, text="Enter Download link:", font=("Arial", 15))
 
-# select path
-path_label = Label(screen, text="Select Path For Download", font=('Arial', 15))
-select_btn =  Button(screen, text="select", command=select_path, font=('Arial', 15))
+#select path for saving the file
+path_label = Label(screen, text="select path for download", font=("Arial", 15))
+select_btn = Button(screen, text="Select Path", bg="red", padx="22", pady="5", font=("Arial", 15), fg="#fff", command=select_path)
 
 #Add to window
-canvas.create_window(250, 280, window=path_label)
+canvas.creat_window(250, 280, window=path_label)
 canvas.create_window(250, 330, window=select_btn)
 
-#Add widgets to window 
+#add widgets to window
 canvas.create_window(250, 170, window=link_label)
 canvas.create_window(250, 220, window=link_field)
 
-#Download btns
-download_btn = Button(screen, text="Download File", font=('Arial', 15))
+#download buttons
+download_btn = Button(screen, text="Download File", bg="green", padx="22", pady="5", font=("Arial", 15), fg="#fff", command=download_file)
+
 #add to canvas
 canvas.create_window(250, 390, window=download_btn)
 
