20f2ce2463f2ab064b9e6b2a014d8cdec93da3f5
cheek-codes <cheek.codes@gmail.com>
2023-03-21 23:25:01 -0400

    updated cipher

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ def download():
         print(f"\nDownloading: {yt.title} ... Wait for it ...")
         
         yd = yt.streams.get_highest_resolution()
-        yd.download("/Users/sandy/Downloads")
+        yd.download("E:/video-downloaded")
 
         print("\nDone\n")
 
         i_finished = input("If you're done, just type \"done\". If not done, press any key and then ENTER ").lower()
         if i_finished == "done":
             quit()
+
 download()
